d74a92d097987994edc55c70c591a688b22009e3
Ruslan Mansurov <mansurov87@gmail.com>
2020-08-13 14:28:34 +0300

    убирает тестовый проект NKH

--- NKH-studio/main.js
+++
@@ -1,9 +0,0 @@
-const categories = document.querySelectorAll('.menu__category');
-
-const categoriesOnclick = (evt) => {
-  evt.currentTarget.classList.toggle('menu__category--opened');
-};
-
-categories.forEach((el) => {
-  el.addEventListener('click', categoriesOnclick);
-});
